5a2e796079ed4c80250d592c25f05514263e58ed
kapil_mighani <158256649+kapilmighani@users.noreply.github.com>
2026-02-26 21:34:15 +0530

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
       bg-[#0f0f0f]/70 backdrop-blur-md
       border-b border-white/10"
     >
-      {/* Desktop Menu */}
+      
       <div className="hidden md:flex gap-4 text-white items-center relative">
         
         <a href="#home" className="px-4 py-2 rounded-lg hover:bg-[#333333] transition">
